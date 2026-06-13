23dc717d13170124725da2c7868565a98fb00a39
Naga Rani <22nn1a0551nagarani@gmail.com>
2026-06-13 12:01:07 +0530

    req postsql

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 import dj_database_url
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iqigvi2m8a6l$8#tufwql5c_3hpcplz!t5jwump&!$)z5=z&me'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
@@ -89,10 +89,7 @@ DATABASES = {
 }
 
 
-# DATABASES = {
- #   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#}
-
+DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
