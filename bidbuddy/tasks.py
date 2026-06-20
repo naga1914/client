a20d5a2c7d6b4a29301f9039c4382d8c6d7b7232
Naga Rani <22nn1a0551nagarani@gmail.com>
2026-06-20 18:37:55 +0530

    last

--- bidbuddy/tasks.py
+++ bidbuddy/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
-from .views import process_pdf
 
 @shared_task
 def process_pdf_task(job_id, pdf_path):
+    from .views import process_pdf
     process_pdf(job_id, pdf_path)
